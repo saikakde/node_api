8328f7f4bf0899f5b6602a716e932aadee813e21
saikakde <saikakde121@gmail.com>
2023-10-16 13:46:05 +0530

    fixed deployment errors and some engine changes

--- data/database.js
+++ data/database.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 export const connectDB=()=>{
     mongoose.connect(process.env.MONGO_URI,{
     dbName:'backend2'
-}).then(()=>console.log('db connected')).catch((e)=>console.log(e));
+}).then((c)=>console.log(`db connected with ${c.connection.host}`)).catch((e)=>console.log(e));
 
 }
